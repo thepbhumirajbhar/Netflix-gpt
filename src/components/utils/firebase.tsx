136d1ff93f546df3e7f1a3218e8087a7b7761a31
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-05 22:28:16 +0530

    global auth initialisation

--- src/components/utils/firebase.tsx
+++ src/components/utils/firebase.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,4 +22,4 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 // Initialize Auth and export it
-export const auth = getAuth(app);
+export const auth = getAuth();
