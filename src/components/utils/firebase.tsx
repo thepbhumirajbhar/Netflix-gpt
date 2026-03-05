cdbe0e12ed10e31f88b9dc04224d1d07efcb1d7a
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-05 22:09:04 +0530

    auth setup and export

--- src/components/utils/firebase.tsx
+++ src/components/utils/firebase.tsx
@@ -19,3 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Auth and export it
+export const auth = getAuth(app);
