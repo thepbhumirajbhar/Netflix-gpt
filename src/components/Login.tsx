ac53d4eeb932534c7a8730340c49624e3c5b30b6
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-05 15:11:27 +0530

    add: more fields in signup form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -19,17 +19,27 @@ return(
         src = "https://assets.nflxext.com/ffe/siteui/vlv3/b9448d14-5983-4ffc-a4d6-e22223108466/web/IN-en-20260302-TRIFECTA-perspective_1ef91182-c674-4632-9bec-d185993ab154_large.jpg"/>
 
      <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col gap-1 bg-black/80 w-3/12 h-7/12 p-10">
+
         <h1 className="text-white text-3xl font-bold pb-4">
           {isSignInForm ? 'Sign In' : 'Sign Up'}
         </h1>
+
+        {!isSignInForm && (
+          <input 
+          type="text"
+          className="my-2 p-3 pl-5 text-sm bg-gray-800 text-white rounded-sm"
+          placeholder="Name" /> )}
+
         <input 
           type="text"
           className="my-2 p-3 pl-5 text-sm bg-gray-800 text-white rounded-sm"
           placeholder="Email or phone number" /> 
+
         <input
-          type="text"
+          type="password"
           className="my-2 p-3 pl-5 text-sm bg-gray-800 text-white rounded-sm"
           placeholder="Password"/>
+
         <button
           className="my-5 p-2 bg-red-700 text-white rounded-sm">
             {isSignInForm ? 'Sign In' : 'Sign Up'}
