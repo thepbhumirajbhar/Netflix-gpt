c955b972ba0ff190afae33de821cc5f297606e40
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 20:46:55 +0530

    feat: trailer pushed to store

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect } from "react";
 import { API_OPTIONS } from "./utils/constants";
+import { useDispatch } from "react-redux";
+import { addTrailerVideo } from "./utils/movieSlice";
 
 interface VideoBgProp {
   movieId: number;
@@ -9,7 +11,9 @@ interface VideoBgProp {
 
 const VideoBG = ({movieId}: VideoBgProp) => {
 
+  const dispatch = useDispatch()
   
+
   const getMovieVideos = async () => {
     const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, API_OPTIONS)
     const json = await data.json()
@@ -17,10 +21,13 @@ const VideoBG = ({movieId}: VideoBgProp) => {
 
     const filterData = json.results. filter((video:any) => video.type === "Trailer")
     //console.log("Filtering Trailer from all videos",filterData)
-    
+
        // multiple videos have type = trailer -> take 0th element if no type then 0th element from list 
     const trailer = filterData.length ? filterData[0] : json.results[0];
        console.log(trailer)
+
+    //adding trailer to store
+    dispatch(addTrailerVideo(trailer))   
 
   };
 
