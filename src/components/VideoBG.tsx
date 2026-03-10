352861fc0cb060bc7598f83500d66de954de18c3
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 21:16:04 +0530

    update: exporting the fetch api to custom hook

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -1,48 +1,22 @@
-//TODO: make API call to fetch Trailer. Since getNowPlayingMovies API doesnot have videos
-
-import { useEffect } from "react";
-import { API_OPTIONS } from "./utils/constants";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideo } from "./utils/movieSlice";
+import { useSelector } from "react-redux";
 import type { RootState } from "./utils/appStore";
-
+import useMovieTrailer from "./hooks/useMovieTrailer";
 
 
 interface VideoBgProp {
   movieId: number;
 }
 
-const VideoBG = ({movieId}: VideoBgProp) => {
 
-  const dispatch = useDispatch()
+
+
+const VideoBG = ({movieId}: VideoBgProp) => {
 
   //Getting trailerVideo from store
   const trailerVideo = useSelector((store: RootState)=> store.movies?.trailerVideo)
   
 
-  const getMovieVideos = async () => {
-    const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, API_OPTIONS)
-    const json = await data.json()
-    //console.log(json);
-
-    const filterData = json.results. filter((video:any) => video.type === "Trailer")
-    //console.log("Filtering Trailer from all videos",filterData)
-
-       // multiple videos have type = trailer -> take 0th element if no type then 0th element from list 
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-       //console.log("trailer",trailer)
-
-    //adding trailer to store
-    dispatch(addTrailerVideo(trailer))   
-
-  };
-
-  useEffect(() =>{
-    getMovieVideos();
-  }, [])
-
-
-
+  useMovieTrailer(movieId);
 
 
   return (
