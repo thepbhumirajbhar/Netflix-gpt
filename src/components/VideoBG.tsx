dc3db8acbb464d758269f15d190bbaa0aa30045a
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 20:15:22 +0530

    initial push

--- src/components/VideoBG.tsx
+++ src/components/VideoBG.tsx
@@ -1,6 +1,41 @@
-const VideoBG = () => {
+//TODO: make API call to fetch Trailer. Since getNowPlayingMovies API doesnot have videos
+
+import { useEffect } from "react";
+import { API_OPTIONS } from "./utils/constants";
+
+interface VideoBgProp {
+  movieId: number;
+}
+
+const VideoBG = ({movieId}: VideoBgProp) => {
+
+  
+  const getMovieVideos = async () => {
+    const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, API_OPTIONS)
+    const json = await data.json()
+    //console.log(json);
+
+    const filterData = json.results. filter((video:any) => video.type === "Trailer")
+    console.log("Filtering Trailer from all videos",filterData)
+       // if multiple videos have type = trailer
+    const trailer = filterData[0];
+
+  };
+
+  useEffect(() =>{
+    getMovieVideos();
+  }, [])
+
+
+
+
+
+
+
   return (
-    <div>Video</div>
+    <div>
+     <h1>hi</h1>
+    </div>
   )
 }
 
