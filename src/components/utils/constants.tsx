c845b88a98a96f6b65aa135224fd992cc6db3846
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 17:07:16 +0530

    initial push

--- src/components/utils/constants.tsx
+++ src/components/utils/constants.tsx
@@ -7,10 +7,4 @@ export const HOME_BG_IMAGE = "https://assets.nflxext.com/ffe/siteui/vlv3/b9448d1
 export const USER_ICON = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
 
 
-export const API_OPTIONS =  {
-                              method: 'GET',
-                              headers: {
-                                accept: 'application/json',
-                                Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYTQ5ZjI2ZWFiYzIwNGFmYzI3Y2EyMWQxODZhMTI2MyIsIm5iZiI6MTc3MzE0MTgyNS43NzYsInN1YiI6IjY5YWZmZjQxOTFhMjIxM2E4NDRlYmFhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rPqckN8tLWPKe-jEVDiprkgzl23YhzP-aqNTO4fV3yk'
-                              }
-                            };                            
+                         
