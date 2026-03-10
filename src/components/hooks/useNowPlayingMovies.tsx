55932c1efe9bab37499a8c004f134fcdcba79d27
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 18:49:17 +0530

    custom hook created

---
+++ src/components/hooks/useNowPlayingMovies.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { addNowPlayingMovies } from "../utils/movieSlice";
+import { API_OPTIONS } from "../utils/constants"
+import { useDispatch } from "react-redux";
+
+
+
+//PURPOSE : fetch data from TMDB API and update store
+
+const useNowPlayingMovies = () => {
+
+  const disptach = useDispatch();
+
+  const getNowPlayingMovies = async() =>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing', API_OPTIONS);
+
+    const json = await data.json();
+    console.log(json.results)
+
+    //pushing movieData to the store
+    disptach(addNowPlayingMovies(json.results));
+
+ };
+
+ useEffect(() => {
+  getNowPlayingMovies();
+ }, [])
+
+}
+
+
+export default useNowPlayingMovies;
