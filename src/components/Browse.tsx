944200b2b85798a7c286838fb26e54de4edf3560
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 18:31:39 +0530

    feat: nowPalyingmovies API call

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import { API_OPTIONS } from "./utils/constants";
+
+
+
 
 const Browse = () => {
+
+ const getNowPlayingMovies = async() =>{
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing', API_OPTIONS);
+
+    const json = await data.json();
+    console.log(json.results)
+
+}  ;
+
+
+ useEffect(() => {
+  getNowPlayingMovies();
+ }, [])
+
+
+
+
 return(
   <div> 
     <Header/>
