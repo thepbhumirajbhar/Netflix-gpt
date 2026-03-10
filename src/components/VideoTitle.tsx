ba1a535e74d4db61df31a5ebb286329dd34933de
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-10 19:40:07 +0530

    update: btns added

--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -1,8 +1,29 @@
-const VideoTitle = () => {
+interface VideoTitleProps {
+  title: string;
+  overview: string;
+}
+
+
+
+
+const VideoTitle = ({title, overview}: VideoTitleProps) => {
   return (
-    <div>Video</div>
+    <div className="pt-36 px-12">
+      <h1 className="font-bold text-6xl"> {title} </h1>
+      <p className="py-6 text-lg w-1/3"> {overview} </p>
+
+      <div className="">
+        <button className="bg-gray-500/40 text-white p-4 px-12 text-xl rounded-lg mx-2"> 
+          ▶️ Play
+        </button>
+        <button className="bg-gray-500/40 text-white p-4 px-12 text-xl rounded-lg">
+          More Info
+        </button>
+      </div>
+      
+    </div>
   )
 }
 
 
-export default VideoTitle;
+export default VideoTitle; 
