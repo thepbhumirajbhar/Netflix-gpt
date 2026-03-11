4b641cebf76f00d377deabfc7f843e048531ef7e
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-11 16:24:34 +0530

    feat(ui): styling movielist

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -14,10 +14,10 @@ const MovieList = ({title, movies}: MovieListProps) => {
   console.log("MovieList: ",movies)
 
   return (
-    <div>
-      <div>
-        <h1>{title}</h1>
-        <div>
+    <div className="bg-black px-6">
+      <h1 className="text-xl text-white pb-2 pt-7">{title}</h1>
+      <div className="flex overflow-x-scroll">
+        <div className="flex gap-5">
           {movies.map((movie) => (
             <MovieCard key = {movie.id} posterPath={movie.poster_path} />
           ))}
