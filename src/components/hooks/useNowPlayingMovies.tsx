f57335dd43bbeed202b2169e025b0954205db7f5
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-11 16:40:42 +0530

    .

--- src/components/hooks/useNowPlayingMovies.tsx
+++ src/components/hooks/useNowPlayingMovies.tsx
@@ -15,7 +15,7 @@ const useNowPlayingMovies = () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing', API_OPTIONS);
 
     const json = await data.json();
-    console.log(json.results)
+    //console.log(json.results)
 
     //pushing movieData to the store
     disptach(addNowPlayingMovies(json.results));
