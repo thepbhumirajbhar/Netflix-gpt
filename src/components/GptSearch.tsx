74e58319a9ba2d485a57531648462de1e9f4a5e2
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-11 21:06:46 +0530

    feat(ui): gpt search components

--- src/components/GptSearch.tsx
+++ src/components/GptSearch.tsx
@@ -1,6 +1,20 @@
+import GptMovieSuggestions from "./GptMovieSuggestions";
+import GptSearchBar from "./GptSearchBar";
+import { HOME_BG_IMAGE } from "./utils/constants";
+
+
+
+
 const GptSearch = () => {
   return (
-    <div></div>
+    <div>
+       <div className="absolute -z-10 ">
+            <img
+              src = {HOME_BG_IMAGE}/>
+        </div>      
+      <GptSearchBar/>
+      <GptMovieSuggestions/>
+    </div>
   )
 }
 
