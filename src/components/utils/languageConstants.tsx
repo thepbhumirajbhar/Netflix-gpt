85c27b3271fbe2a7d9ce7d1976fa503433b35fbd
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-12 01:09:01 +0530

    feat: build multi language page

---
+++ src/components/utils/languageConstants.tsx
@@ -0,0 +1,17 @@
+const lang = {
+  en:{
+    search: "Search",
+    gptPlaceHolderText: "What would you like to watch today..?"
+  },
+  hindi:{
+    search:"खोज",
+    gptPlaceHolderText: "आज आप क्या देखना चाहेंगे...?"
+  },
+  spanish:{
+    search: "buscar",
+    gptPlaceHolderText: "¿Qué te gustaría ver hoy?"
+  }
+}
+
+
+export default lang;
