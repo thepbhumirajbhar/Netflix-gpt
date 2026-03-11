7a143c3defe1131738d1d7866d7901e509b762f6
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-11 21:07:17 +0530

    feat(ui): btn and input for search

--- src/components/GptSearchBar.tsx
+++ src/components/GptSearchBar.tsx
@@ -1,6 +1,23 @@
+
+
+
+
 const GptSearchBar = () => {
   return (
-    <div></div>
+    <div className="pt-[12%] flex justify-center">
+      <form className= "bg-black grid grid-cols-12 w-1/2 ">
+
+        <input 
+          type="text" 
+          className="m-2 p-2 bg-amber-50 col-span-9"
+          placeholder="What would you like to watch today..?"/>
+
+        <button className="px-2 m-2 bg-red-700 text-white rounded-md col-span-3">
+          Search
+        </button>
+
+      </form>
+    </div>
   )
 }
 
