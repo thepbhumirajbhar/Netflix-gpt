0eb711a2fef781100ff2981adb79ef612996d4d2
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-12 01:44:57 +0530

    feat: multi lang functionality

--- src/components/GptSearchBar.tsx
+++ src/components/GptSearchBar.tsx
@@ -1,8 +1,13 @@
+import type { RootState } from "./utils/appStore";
 import lang from "./utils/languageConstants";
+import { useSelector } from "react-redux";
 
 
 
 const GptSearchBar = () => {
+
+  const langKey = useSelector((store: RootState) => store.config.lang) as keyof typeof lang;
+
   return (
     <div className="pt-[12%] flex justify-center">
       <form className= "bg-black grid grid-cols-12 w-1/2 ">
@@ -10,10 +15,10 @@ const GptSearchBar = () => {
         <input 
           type="text" 
           className="m-2 p-2 bg-amber-50 col-span-9"
-          placeholder={lang.hindi.gptPlaceHolderText}/>
+          placeholder={lang[langKey].gptPlaceHolderText}/>
 
         <button className="px-2 m-2 bg-red-700 text-white rounded-md col-span-3">
-          {lang.hindi.search}
+          {lang[langKey].search}
         </button>
 
       </form>
