8092e2d6c44827a0106578617c98ecdba19d32fd
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-11 20:12:32 +0530

    feat: gpt page toggle functionality

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -1,14 +1,20 @@
+import GptSearch from "./GptSearch";
 import Header from "./Header";
 import useNowPlayingMovies from "./hooks/useNowPlayingMovies";
 import usePopularMovies from "./hooks/usePopularMovies";
 import useTopRatedMovies from "./hooks/useTopRatedMovies";
 import MainContainer from "./MainContainer";
 import SecondaryContainer from "./SecondaryContainer";
+import type { RootState } from "./utils/appStore";
+import { useSelector } from "react-redux";
 
 
 
 
 const Browse = () => {
+
+  // getting status of showGptSearch from store -> if true: show gpt page otherwise no
+  const showGptSearch = useSelector((store: RootState)=> store.gpt.showGptSearch)
 
  useNowPlayingMovies();
  usePopularMovies();
@@ -17,8 +23,16 @@ const Browse = () => {
 return(
   <div> 
     <Header/>
-    <MainContainer />
-    <SecondaryContainer />
+    {/* //if true: show gpt page otherwise no */}
+    {
+      showGptSearch ? <GptSearch /> : 
+                                      <>
+                                        <MainContainer />
+                                        <SecondaryContainer />
+                                      </>
+    }
+    
+    
 
     {/*
        Main Container 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,6 +9,7 @@ import type { RootState } from "./utils/appStore";
 import { useEffect } from "react";
 import {onAuthStateChanged } from "firebase/auth";
 import { addUser, removeUser } from "./utils/userSlice";
+import { toggleGptSearchView } from "./utils/gptSlice";
 
 
 
@@ -73,6 +74,17 @@ const Header = () => {
   //console.log("Current user from Redux:", user);
 
 
+
+
+
+
+
+
+  const handleGptSearchClick = () => {
+    // Toggle GPT Search
+    dispatch(toggleGptSearchView())
+  }
+
   
 
   return(
@@ -87,7 +99,8 @@ const Header = () => {
       {user?.uid && (
         <div className="flex gap-2 items-center">
         
-          <button className="text-black text-sm gap-1 px-2 py-1 m-6 flex bg-gray-300 rounded-md">
+          <button className="text-black text-sm gap-1 px-2 py-1 m-6 flex bg-gray-300 rounded-md"
+                  onClick={handleGptSearchClick}>
 
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="size-4">
             <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
