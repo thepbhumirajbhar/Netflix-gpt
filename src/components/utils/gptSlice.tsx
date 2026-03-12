556dd9742625e23320a9bd33fcc12e5168b36d9a
bhumirajbhar <bhumirajbhar60@gmail.com>
2026-03-12 20:25:24 +0530

    feat: add movie results to the slice

--- src/components/utils/gptSlice.tsx
+++ src/components/utils/gptSlice.tsx
@@ -3,15 +3,19 @@ import { createSlice } from "@reduxjs/toolkit";
 const gptSlice = createSlice({
   name: "gpt",
   initialState: {
-    showGptSearch: false
+    showGptSearch: false,
+    gptMovies: null
   },
   reducers:{
     toggleGptSearchView: (state) => {
       state.showGptSearch = !state.showGptSearch;
+    },
+    addGptMovieResult: (state, action) => {
+      state.gptMovies = action.payload
     }
   }
 })
 
 
-export const {toggleGptSearchView} = gptSlice.actions;
-export default gptSlice.reducer;
+export const {toggleGptSearchView, addGptMovieResult} = gptSlice.actions;
+export default gptSlice.reducer; 
